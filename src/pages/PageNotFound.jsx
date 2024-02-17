3207fea94705e3c2cf5f52e4ff8fccdf9a485645
Nick Neessen <nick.neessen@gmail.com>
2024-02-17 13:02:45 -0600

    added more componenets and styles modules

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-import PageNav from "../components/PageNav";
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
