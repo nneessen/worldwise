25a9d141eca3ccb19e5b25b2b3731d84edfc21e3
Nick Neessen <nick.neessen@gmail.com>
2024-02-17 13:36:42 -0600

    a

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<h1>List of cities</h1>} />
+          <Route path="cities" element={<h1>List of cities</h1>} />
+          <Route path="countries" element={<h1>List of countries</h1>} />
+          <Route path="form" element={<h1>Form</h1>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
-        <Route path="app" element={<AppLayout />} />
       </Routes>
     </BrowserRouter>
   );
