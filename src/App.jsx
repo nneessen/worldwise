1db6757efc172d5a01bd103acad6ba0e3463011c
Nick Neessen <nick.neessen@gmail.com>
2024-02-17 15:24:52 -0600

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [cities, setCities] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +41,14 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
           <Route index element={<h1>List of cities</h1>} />
-          <Route path="cities" element={<h1>List of cities</h1>} />
-          <Route path="countries" element={<h1>List of countries</h1>} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="countries"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<h1>Form</h1>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
