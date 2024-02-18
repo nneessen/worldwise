9daee687b07ae9df519ca697faebf76122e4e3b3
Nick Neessen <nick.neessen@gmail.com>
2024-02-18 17:31:56 -0600

    implement delete city function

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -55,6 +55,20 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error deleting city");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
@@ -63,6 +77,7 @@ function CitiesProvider({ children }) {
         currentCity,
         getCity,
         createCity,
+        deleteCity,
       }}
     >
       {children}
