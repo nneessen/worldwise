a0ec09d49d098ec459ead437fedd0d4a11fe7f00
Nick Neessen <nick.neessen@gmail.com>
2024-02-18 11:32:33 -0600

    create useCities ContextProvider

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,11 +2,13 @@
 /* eslint-disable no-unused-vars */
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
-import CityItem from "./CityItem";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-export default function CountryList({ cities, isLoading }) {
+export default function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
